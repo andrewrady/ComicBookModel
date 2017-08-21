393fdc0db7bbd1d36a47e06cf4c19c03295a6c0b
Andrew Rady <andrew.rady@ritterim.com>
2017-08-20 23:34:58 -0600

    added single to many relationship with series and comicbook

--- ComicBookGalleryModel/Models/ComicBook.cs
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -9,10 +9,21 @@ namespace ComicBookGalleryModel.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public int SeriesId { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public decimal? AverageRating { get; set; }
+
+        public Series Series { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Series?.Title} #{IssueNumber}";
+            }
+        }
+
     }
 }
